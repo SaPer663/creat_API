48bcf3ed92b562a0df5847719a30eba3fec562db
saper663_home <cheblin@yandex.ru>
2018-12-09 21:36:30 +0500

    latest version

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const app = express();
 
 const port = 8000;
 require('./app/routes')(app, {});
+app.use(bodyParser.urlencoded({ extended: true}));
 app.listen(port, () => {
   console.log(`We are living on ${port}`)
 });
-module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    res.send('Hi!')
-  });
-};

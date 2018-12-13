999a1dc0b7343c0afc8e567ddbbbbb7d2c51d414
saper663_home <cheblin@yandex.ru>
2018-12-13 12:54:41 +0500

    add Get notes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -5,11 +5,18 @@ module.exports = function(app, client) {
   app.get('/notes/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectId(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': 'An error has occured'});
+      } else {
+        res.send(item);
+      }
+    });
   });
 
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
-    db.collection('notes').(note, (err, result) => {
+    db.collection('notes').insert(note, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occured' });
       } else {

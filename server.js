5f8f27a1b2dbf3fed1d12e5b092d00b5b021e531
saper663_work <cheblin@yandex.ru>
2018-12-15 23:02:43 +0500

    fix server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
 const port = 8000;
-app.use(bodyParser.urlencoded({ extended: true}));
+/* app.use(bodyParser.urlencoded({ extended: true}));
 MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
   if (err) {
     return console.log(err);
@@ -15,4 +15,24 @@ MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
   app.listen(port, () => {
     console.log(`We are living on ${port}`)
   });
+});
+*/
+/* app.use(function (request, response) {
+  const adress = __dirname + "/public/index.html";
+  console.log(adress);
+  response.sendFile(adress);
+}); */
+
+app.use(express.static('public'));
+app.listen(port, () => {
+  console.log(`We are living on ${port}`)
+});
+// app.use(express.static('public'));
+const adressMiddle = __dirname + '/public/middle.html';
+const adressHard = __dirname + '/public/hard.html';
+app.get('/middle', (req, res) => {
+  res.sendFile(adressMiddle);
+});
+app.get('/hard', (req, res) => {
+  res.sendFile(adressHard);
 });
